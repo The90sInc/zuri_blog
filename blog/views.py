13f28a2901a0bcf72a6b927d9332563270c9b633
techguru <niyotokunlola@gmail.com>
2021-04-27 23:11:38 +0100

    uncompleted blog ass

---
+++ blog/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import SignUpForm
+
+
+# Create your views here.
+
+# Sign Up View
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+
+class blogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
+
+
