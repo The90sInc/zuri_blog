0d587a4a389920a21942fa244603b30988b27bbe
techguru <niyotokunlola@gmail.com>
2021-04-29 19:00:36 +0100

    deploying app to heroku

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 import os
 from pathlib import Path
 
@@ -132,3 +132,5 @@ LOGOUT_REDIRECT_URL = 'login' # new
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
+django_heroku.settings(locals())
